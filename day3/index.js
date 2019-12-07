d1d70a5ec4b9c16b2d7e82750ca89a3d61135ad9
niemisami <sami.nieminen@madbooster.com>
2019-12-07 11:02:55 +0200

    Rewrite day3 step 1 implementation

--- day3/index.js
+++ day3/index.js
@@ -4,20 +4,12 @@ const inputFile = 'input.txt'
 const wires = fs.readFileSync(`${__dirname}/${inputFile}`, 'utf-8')
   .split(/\r?\n/).map(line => line.split(','))
 
-
 // Test inputs
 const wire1_1 = ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72']
 const wire1_2 = ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83'] //distance 159
 
 const wire2_1 = ['R98', 'U47', 'R26', 'D63', 'R33', 'U87', 'L62', 'D20', 'R33', 'U53', 'R51']
 const wire2_2 = ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7'] // = distance 135
-
-// step 1 Convert directions into points
-// step 2 Find the same points
-// step 3 Calculate Manhattan distance
-
-// It doesn't matter on which point the two wires starts from
-// Therefore use the 0,0
 
 const directionMap = {
   U: (previousPoint, length) => ({ x: previousPoint.x, y: previousPoint.y + length }),
@@ -28,32 +20,60 @@ const directionMap = {
 
 const calculateCoordinates = (previousPoint, direction, length) => directionMap[direction](previousPoint, length)
 
+const isPointBetweenLine = (axis, point, bStart, bEnd) =>
+  Math.min(bStart[axis], bEnd[axis]) <= point[axis] && point[axis] <= Math.max(bStart[axis], bEnd[axis])
 
+const getIntersectionPoint = (aStart, aEnd, bStart, bEnd) => {
+  if(isPointBetweenLine('y', aStart, bStart, bEnd) && isPointBetweenLine('x', bStart, aStart, aEnd)) {
+    return { x: bStart.x, y: aStart.y }
+  } else if(isPointBetweenLine('y', bStart, aStart, aEnd) && isPointBetweenLine('x', aStart, bStart, bEnd)) {
+    return { x: aStart.x, y: bStart.y }
+  }
+  return null
+}
+
+// // It doesn't matter on which point the two wires starts from
+// // Therefore use the 0,0
 const initialPoint = { x: 0, y: 0 }
-
-const directionsToIndividualPoints = directionArray => directionArray.reduce((acc, path) => {
+const directionsToPoints = directionArray => directionArray.reduce((acc, path, index) => {
+  const previousPoint = acc[index - 1] || initialPoint
   const direction = path[0]
   const length = Number(path.slice(1))
-  const increment = 1
-  // Convert each direction to individual points and store in an array
-  Array(length).fill(0).forEach((_) => {
-    const previousPoint = acc[acc.length - 1] || initialPoint
-    const coordinates = calculateCoordinates(previousPoint, direction, increment)
-    acc.push(coordinates)
-  })
+  const coordinates = calculateCoordinates(previousPoint, direction, length)
+  acc.push(coordinates)
   return acc
 }, [])
 
-const manhattanDistance = (pointA = initialPoint, pointB = initialPoint) =>
+const wireA = directionsToPoints(wires[0])
+const wireB = directionsToPoints(wires[1])
+
+// Tests
+// const wireA = directionsToPoints(wire2_1)
+// const wireB = directionsToPoints(wire2_2)
+
+// const aS = { x: 13, y: 100 }
+// const aE = { x: 13, y: -100 }
+
+// const bS = { x: 100, y: 20 }
+// const bE = { x: -1, y: 20 }
+// console.log(getIntersectionPoint(aS, aE, bS, bE))
+
+const intersections = wireA.reduce((intersections, pointA, index) => {
+  const previousPoint = wireA[index - 1]
+  previousPoint && wireB.forEach((pointB, bIndex) => { // First value is always undefined
+    const previousPointB = wireB[bIndex - 1]
+    if(previousPointB) {
+      const intersection = getIntersectionPoint(previousPoint, pointA, previousPointB, pointB)
+      intersection && intersections.push(intersection)
+    }
+  })
+  return intersections
+}, [])
+
+const manhattanDistance = (pointA, pointB) =>
   Math.abs(pointA.x - pointB.x) + Math.abs(pointA.y - pointB.y)
 
-const wireA = directionsToIndividualPoints(wires[0])
-const wireB = directionsToIndividualPoints(wires[1])
-
-const junctionPoints = wireA.filter(pointA => wireB.find(pointB => pointA.x === pointB.x && pointA.y === pointB.y))
-
-console.log('Cross junctions', junctionPoints)
-const shortestManhattanDistance = junctionPoints.reduce((lastDistance, point) => {
+const shortestManhattanDistance = intersections.reduce((lastDistance, point) => {
   const distance = manhattanDistance(initialPoint, point)
   return lastDistance > distance ? distance : lastDistance
 }, Number.MAX_SAFE_INTEGER)
